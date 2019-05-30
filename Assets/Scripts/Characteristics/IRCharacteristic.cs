c8839dff1d1a36bcb4eb355d11674a27fd840265
dangpg <dangpg@users.noreply.github.com>
2019-05-30 22:41:42 +0200

    First working prototype

---
+++ Assets/Scripts/Characteristics/IRCharacteristic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class IRCharacteristic : BaseCharacteristic<bool>
+{
+    public IRCharacteristic() : base("11111111-1111-4b23-9358-f235b978d07c", false)
+    {
+    }
+
+    public override void UpdateValue(byte[] bytes)
+    {
+        if (bytes != null)
+        {
+            this.Value = BitConverter.ToInt16(bytes, 0) == 0 ? false : true;
+        }
+    }
+}
---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    private Renderer _cubeRenderer;
+    private BLEManagerScript _bleManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cubeRenderer = GetComponent<Renderer>();
+        _bleManager = GameObject.Find("BLEManager").GetComponent<BLEManagerScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_bleManager.GetCharacteristic<IRCharacteristic>().Value)
+        {
+            _cubeRenderer.material.color = Color.green;
+        }
+        else
+        {
+            _cubeRenderer.material.color = Color.red;
+        }
+    }
+}
